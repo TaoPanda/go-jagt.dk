074936619cfa729694b042c4a715585de0affbb6
TaoPanda <erik2708@campusvajle.dk>
2021-05-27 14:55:13 +0200

    started work on om-os subside

--- om-os.php
+++ om-os.php
@@ -1,11 +1,15 @@
 <?php 
-    $css = "forside";
+    $css = "om-os";
     $title = "Om os";
     include "./includes/header.php";
     include "./includes/nav.php";
 ?>
 <main>
-
+<img src="https://source.unsplash.com/1400x900/?hunting" alt="">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit qui veritatis aut, fugiat nam aperiam animi maxime repellendus unde libero, dolor at quas mollitia facilis quibusdam dicta delectus minus quos.
+Vero voluptatem quidem consequuntur, laboriosam ea voluptatum nulla repudiandae, porro corrupti, dolore similique nam alias hic corporis iste dicta et eligendi voluptas. Ipsam exercitationem praesentium, repudiandae modi fuga debitis explicabo?
+Quos aspernatur id, maiores aut assumenda quis similique sit hic, dolorum minima iste quasi nulla laboriosam quae dicta, consequatur placeat deserunt magnam dolor repellat nesciunt animi dignissimos! Voluptatibus, harum nemo?</p>
+<img src="https://source.unsplash.com/1400x900/?hunting" alt="">
 </main>
 <?php
     include "./includes/footer.php";
